6536ef22d2a87911db30a67e9f13a90cd260b047
ぐろてん <milien.groze@gmail.com>
2018-03-17 13:07:21 +0900

    test

--- ahnw/routes/mongo.js
+++ ahnw/routes/mongo.js
@@ -7,8 +7,8 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://tk2-410-46049.vs.sakura.ne.jp/otukare';
-// var url = 'mongodb://localhost:27017/otukare';
+// var url = 'mongodb://tk2-410-46049.vs.sakura.ne.jp/otukare';
+var url = 'mongodb://localhost:27017/otukare';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, mongodb) {
